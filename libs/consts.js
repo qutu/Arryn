1da5bd3efa28682e9f0c9964ca3a1eed8805d7c6
Guo Yu <o.u.turing@gmail.com>
2015-12-03 02:58:47 +0800

    Fullfill routes

--- libs/consts.js
+++ libs/consts.js
@@ -1,4 +1,3 @@
 export default {
   API_ENDPOINT: '',
-  API_VERSION: '',
 }
--- libs/routes.js
+++ libs/routes.js
@@ -1,7 +1,54 @@
 import scenic from '../components/scenic.vue'
+import scenicHome from '../components/scenic/home.vue'
+import scenicSpots from '../components/scenic/spots.vue'
+import scenicSpotsShopping from '../components/scenic/shopping.vue'
+
+import spot from '../components/spot.vue'
+import spotHome from '../components/spot/home.vue'
+
+import map from '../components/map.vue'
+import photos from '../components/photos.vue'
 
 export default {
   '/scenics/:id': {
-    component: scenic
+    component: scenic,
+    subRoutes: {
+      '/': {
+        component: scenicHome
+      },
+      '/shopping': {
+        component: scenicSpotsShopping
+      },
+      '/photos': {
+        component: photos
+      },
+      '/map': {
+        component: map
+      },
+      '/spots': {
+        component: {
+          template: '<router-view></router-view>'
+        },
+        subRoutes: {
+          '/': {
+            component: scenicSpots,
+          },
+          '/:spotId': {
+            component: spot,
+            subRoutes: {
+              '/': {
+                component: spotHome
+              },
+              '/photos': {
+                component: photos
+              },
+              '/map': {
+                component: map
+              }
+            }
+          }
+        }
+      }
+    }
   }
 }
