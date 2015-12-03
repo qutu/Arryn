366d36485262f8ada71dc76e4a2b2930e4b16fd1
Guo Yu <o.u.turing@gmail.com>
2015-12-04 03:08:51 +0800

    Add component tour

---
+++ components/tour.vue
@@ -0,0 +1,42 @@
+<template>
+  <!-- todo: this scenic.header component would be repeated -->
+  <header class="scenic-header clearfix" 
+    v-bind:style="{ 'background-image': cover }">
+    <h1 class="scenic-title">{{ scenic.name }} <span class="level">{{ scenic.level }}A</span></h1>
+    <a v-link="{ path: '../' + $route.params.id + '/map' }" class="scenic-map-button">Map</a>
+  </header>
+  <nav class="scenic-nav">
+    <ul class="clearfix">
+      <li class="spot-item"><a v-link="{ path: '../' + $route.params.id + '/spots' }">景点</a></li>
+      <li class="shopping-item"><a v-link="{ path: '../' + $route.params.id + '/shopping' }">设施</a></li>
+      <li class="tour-item current-item"><a v-link="{ path: $route.path }">导览</a></li>
+    </ul>
+  </nav>
+  <section class="scenic-section section-tour">
+    <img v-bind:src="scenic.guidemap_url" alt="">
+  </section>
+</template>
+
+<script>
+  import { Get } from '../libs/api'
+
+  export default {
+    data() {
+      return {
+        err: null,
+        scenic: {},
+        cover: null,
+      }
+    },
+    created() {
+      Get(`scenics/${this.$route.params.id}`)
+        .then(result => {
+          this.scenic = result
+
+          if (result.main_img_url)
+            this.cover = `url(${result.main_img_url})`
+        })
+        .catch(err => this.err = err)
+    }
+  }
+</script>
