d29230cd87cb6ec19b5470e04582332348e6c6f7
Guo Yu <o.u.turing@gmail.com>
2015-12-09 00:42:49 +0800

    Enable image viewer

--- components/photos.vue
+++ components/photos.vue
@@ -1,20 +1,30 @@
 <template>
   <header class="sub-header">
-    <a v-link="{ path: goBack() }" class="back">&lt; {{name}}</a>
+    <a v-link="{ path: goBack() }" 
+      class="back">&lt; {{name}}
+    </a>
   </header>
   <section class="scenic-section photos-section">
     <div class="photos clearfix">
-      <div class="scenic-photo-thumbnail" v-for="photo in photos">
+      <div class="scenic-photo-thumbnail" 
+        @click="openSwipe(photo)"
+        v-for="photo in photos">
         <img v-bind:src="photo.src" alt="">
       </div>
     </div>
   </section>
+  <swipe></swipe>
 </template>
 
 <script>
   import { Get } from '../libs/api'
+  import swipe from './swipe.vue'
+  import Photoswipe from 'photoswipe'
 
   export default {
+    components: {
+      swipe,
+    },
     data() {
       return {
         err: null,
@@ -36,8 +46,7 @@
           if (!!spotId)
             return Get(`spots/${spotId}/photos`)
 
-          if (result.photos)
-            this.photos = result.photos
+          this.photos = result.photos
         })
         .then(photos => this.photos = photos)
         .catch(err => this.err = err)
@@ -48,6 +57,18 @@
         return spotId ? 
           `/scenics/${this.$route.params.id}/spots/${spotId}` :
           `/scenics/${this.$route.params.id}`;
+      },
+      openSwipe(currentPhoto) {
+        const index = currentPhoto ? this.photos.indexOf(currentPhoto) : 0
+        const gallery = new Photoswipe(document.getElementById('pswp'), window.PhotoSwipeUI_Default, this.photos, {
+          history: false,
+          focus: false,
+          loop: true,
+          showAnimationDuration: 0,
+          hideAnimationDuration: 0,
+          index,
+        })
+        gallery.init()
       }
     }
   }
--- libs/app.js
+++ libs/app.js
@@ -3,7 +3,8 @@
 // Global deps
 import 'script!es6-promise-polyfill'
 import 'script!whatwg-fetch'
-import 'script!photoswipe'
+import 'script!photoswipe/dist/photoswipe-ui-default.min.js'
+
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
@@ -13,6 +14,7 @@ import routes from './routes'
 
 // App Styles
 import '../css/app.css'
+import 'style/url!file!../node_modules/photoswipe/dist/default-skin/default-skin.css'
 
 Vue.use(VueRouter)
 
